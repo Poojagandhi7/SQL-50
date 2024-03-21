df3332164132c4a2e2257bec7bece9218fb2fa53
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-03-21 14:37:52 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (33.58%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n = s.length();
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else{
+                    char c = st.top();
+                    st.pop();
+                    if((c == '(' && s[i]==')') ||
+                       (c == '[' && s[i]==']') ||
+                       (c == '{' && s[i]=='}')){
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+};
