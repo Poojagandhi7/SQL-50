7833501396fc1ae789507257a446d2ce7e304e0a
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-04-30 13:40:54 +0530

    Time: 42 ms (6.09%), Space: 28.7 MB (6.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,29 +1,80 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-        int n = nums.size();
         vector<int> reverse;
-        
-       
+        int n = nums.size();
         if(n==1){
-            for(int i=0;i<n;i++){
-            cout << nums[i];
-            }
+            cout<<nums[0];
         }
+        
         if(k>n){
-            k = k % n;
+            k = k%n;
         }
         
         for(int i=n-k;i<n;i++){
             reverse.push_back(nums[i]);
         }
+        
         for(int i=0;i<n-k;i++){
             reverse.push_back(nums[i]);
         }
         nums = reverse;
-        for(int i=0;i<reverse.size();i++){
-            cout << nums[i];
+        for(int i=0;i<n;i++){
+            cout<<nums[i];
         }
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int n = nums.size();
+//         vector<int> reverse;
+        
+       
+//         if(n==1){
+//             for(int i=0;i<n;i++){
+//             cout << nums[i];
+//             }
+//         }
+//         if(k>n){
+//             k = k % n;
+//         }
+        
+//         for(int i=n-k;i<n;i++){
+//             reverse.push_back(nums[i]);
+//         }
+//         for(int i=0;i<n-k;i++){
+//             reverse.push_back(nums[i]);
+//         }
+//         nums = reverse;
+//         for(int i=0;i<reverse.size();i++){
+//             cout << nums[i];
+//         }
