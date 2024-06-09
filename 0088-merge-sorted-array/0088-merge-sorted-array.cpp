250da3996d9c33636ac1ac323eaf5c6edf6f9d7a
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-09 23:20:09 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (6.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -35,25 +35,3 @@ public:
         }
     }
 };
-
-// while (i<n1 && j <n2)
-//     {
-//         // Check if current element of first
-//         // array is smaller than current element
-//         // of second array. If yes, store first
-//         // array element and increment first array
-//         // index. Otherwise do same with second array
-//         if (arr1[i] < arr2[j])
-//             arr3[k++] = arr1[i++];
-//         else
-//             arr3[k++] = arr2[j++];
-//     }
- 
-//     // Store remaining elements of first array
-//     while (i < n1)
-//         arr3[k++] = arr1[i++];
- 
-//     // Store remaining elements of second array
-//     while (j < n2)
-//         arr3[k++] = arr2[j++];
-// }
