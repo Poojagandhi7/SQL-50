75d4cec3a7c0f894df2e06c160437ae98ef0a953
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-10 15:56:46 +0530

    Time: 49 ms (37.65%), Space: 12.7 MB (56.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> b;
-        int n =nums.size();
-        int sum=0;
+        
+        int n = nums.size();
+        
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
-                    b.push_back(i);
-                    b.push_back(j);
+                    return {i,j};
+                    break;
                 }
+                
             }
         }
-        return b;
+        return {};
+        
     }
-    
 };
-
-// class Solution:
-//     def twoSum(self, nums: List[int], target: int) -> List[int]:
-//         result = []
-//         for i in range(0,len(nums)):
-//             for j in range(i+1,len(nums)):
-//                 if(nums[i]+nums[j]==target):
-//                     result.append(i)
-//                     result.append(j)
-//         return result          
-                 
