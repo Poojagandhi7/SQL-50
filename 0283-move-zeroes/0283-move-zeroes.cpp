db84c7d284c289defc4a6d244c24acb4fd973612
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-10 13:05:29 +0530

    Time: 16 ms (54.08%), Space: 21.5 MB (73.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,70 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> b;
-        vector<int> a;
+        int j = -1;
         int n = nums.size();
+        if(n==1){
+            return;
+        }
         for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1){
+            return;
+        }
+        for(int i=j+1;i<n;i++){
             if(nums[i]!=0){
-                a.push_back(nums[i]);
+                swap(nums[i],nums[j]);
+                j++;
             }
-            else if(nums[i]==0){
-                b.push_back(nums[i]);
-            }
-        }
-        
-        for(int i=0;i<b.size();i++){
-            a.push_back(b[i]);
-        }
-        nums=a;
-        for(int i=0;i<n;i++){
-            cout<<nums[i];
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// vector<int> b;
+//         vector<int> a;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=0){
+//                 a.push_back(nums[i]);
+//             }
+//             else if(nums[i]==0){
+//                 b.push_back(nums[i]);
+//             }
+//         }
+        
+//         for(int i=0;i<b.size();i++){
+//             a.push_back(b[i]);
+//         }
+//         nums=a;
+//         for(int i=0;i<n;i++){
+//             cout<<nums[i];
+//         }
