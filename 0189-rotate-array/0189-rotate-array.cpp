ccb1d26b11754297baa32ee28cadf348889eef00
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-10 12:26:04 +0530

    Time: 33 ms (5.98%), Space: 28.8 MB (5.60%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,80 +1,73 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> reverse;
+        vector<int> b;
         int n = nums.size();
+        
         if(n==1){
-            cout<<nums[0];
+            cout << nums[0];
         }
-        
         if(k>n){
-            k = k%n;
+            k = k % n;
         }
-        
         for(int i=n-k;i<n;i++){
-            reverse.push_back(nums[i]);
+            b.push_back(nums[i]);
         }
-        
         for(int i=0;i<n-k;i++){
-            reverse.push_back(nums[i]);
+            b.push_back(nums[i]);
         }
-        nums = reverse;
+        nums=b;
         for(int i=0;i<n;i++){
             cout<<nums[i];
         }
         
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int n = nums.size();
-//         vector<int> reverse;
         
-       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         vector<int> reverse;
+//         int n = nums.size();
 //         if(n==1){
-//             for(int i=0;i<n;i++){
-//             cout << nums[i];
-//             }
+//             cout<<nums[0];
 //         }
+        
 //         if(k>n){
-//             k = k % n;
+//             k = k%n;
 //         }
         
 //         for(int i=n-k;i<n;i++){
 //             reverse.push_back(nums[i]);
 //         }
+        
 //         for(int i=0;i<n-k;i++){
 //             reverse.push_back(nums[i]);
 //         }
 //         nums = reverse;
-//         for(int i=0;i<reverse.size();i++){
-//             cout << nums[i];
+//         for(int i=0;i<n;i++){
+//             cout<<nums[i];
 //         }
+        
+    }
+};
