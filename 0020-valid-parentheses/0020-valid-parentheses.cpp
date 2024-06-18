fe319e8daee2c1b7ceb17df510543d746f3aa1d2
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-18 11:46:19 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (60.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -6,7 +6,7 @@ public:
         int n = s.length();
         
         for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
                 st.push(s[i]);
             }
             else{
@@ -14,22 +14,74 @@ public:
                     return false;
                 }
                 else{
-                    char c = st.top();
-                    st.pop();
-                    if((c == '(' && s[i]==')') ||
-                       (c == '[' && s[i]==']') ||
-                       (c == '{' && s[i]=='}')){
-                        continue;
+                    char c = s[i];
+                    if(st.top()=='(' && c==')' ||
+                       st.top()=='[' && c==']' || 
+                       st.top()=='{' && c=='}')
+                    {
+                        st.pop();
                     }
                     else{
-                        return false;
+                    return false;
                     }
-                }
+                    }
+                
             }
         }
         if(st.empty()){
             return true;
         }
-        return false;
+        else{
+            return false;
+        }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// stack<char> st;
+//         int n = s.length();
+        
+//         for(int i=0;i<n;i++){
+//             if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 if(st.empty()){
+//                     return false;
+//                 }
+//                 else{
+//                     char c = st.top();
+//                     st.pop();
+//                     if((c == '(' && s[i]==')') ||
+//                        (c == '[' && s[i]==']') ||
+//                        (c == '{' && s[i]=='}')){
+//                         continue;
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+//         if(st.empty()){
+//             return true;
+//         }
+//         return false;
