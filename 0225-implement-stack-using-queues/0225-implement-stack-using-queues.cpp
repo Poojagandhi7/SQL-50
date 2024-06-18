e1dafefe5732128d3c9e56688d61c0d1c86f2beb
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-18 11:09:32 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (74.94%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -9,6 +9,13 @@ public:
     }
     
     void push(int x) {
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // swap(q1,q2);
+        
         q2.push(x);
         while(!q1.empty()){
             q2.push(q1.front());
@@ -19,10 +26,17 @@ public:
     }
     
     int pop() {
+        // if(q1.empty()){
+        //     return -1;
+        // }
+        // int temp = q1.front();
+        // q1.pop();
+        // return temp;
+        
         if(q1.empty()){
             return -1;
         }
-        int temp = q1.front();
+        int temp=q1.front();
         q1.pop();
         return temp;
     }
@@ -55,46 +69,3 @@ public:
  * bool param_4 = obj->empty();
  */
 
-/* class MyStack {
-    queue<int>q1;
-    queue<int>q2;
-public:
-    MyStack() {
-        
-    }
-    
-    void push(int x) {
-       q2.push(x);
-       while(!q1.empty())
-       {
-           q2.push(q1.front());
-           q1.pop();
-       }
-       swap(q1,q2);
-    }
-    
-    int pop() {
-        if(q1.empty())
-        return -1;
-       int ans=q1.front();
-       q1.pop();
-        return ans; 
-        
-    }
-    
-    int top() {
-        if(!q1.empty())
-        return q1.front();
-        else
-        return -1;
-        
-    }
-    
-    bool empty() 
-    {
-        if(q1.empty())
-        return true;
-        return false;
-        
-    }
-};*/
