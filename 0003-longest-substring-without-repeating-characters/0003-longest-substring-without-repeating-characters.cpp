9fd66e0726092474e347c9849f65af4f3b6f6880
Poojagandhi7 <130622490+Poojagandhi7@users.noreply.github.com>
2024-06-20 20:50:37 +0530

    Time: 25 ms (24.10%), Space: 12.8 MB (36.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,78 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    int n = s.length();
-    map<char,int> mpp;
-    int min = 0;
-    int i=0,j=0;
-
-    for(int j = 0 ; j<n ; j++){
-        mpp[s[j]]++;
+        int n = s.length();
+        map<char,int> mpp;
         
+        int i=0;
+        int j = 0;
         
-        while(mpp.size() < j-i+1){
+        int min = 0;
+        
+        for(j=0;j<n;j++){
+            mpp[s[j]]++;
+            
+            
+            while(mpp.size()<j-i+1){
                 mpp[s[i]]--;
-                if (mpp[s[i]] == 0) {
-                  mpp.erase(s[i]);
+                if(mpp[s[i]]==0){
+                    mpp.erase(s[i]);
                 }
                 i++;
             }
-        
-
-        if(mpp.size()==j-i+1){
-            min=max(min,j-i+1);
+            if(mpp.size()==j-i+1){
+                min = max(min,j-i+1);
+            }
+            
         }
         
-    }
-    
-    return min;
+        return min;
         
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// map<char,int> mpp;
+//     int min = 0;
+//     int i=0,j=0;
+
+//     for(int j = 0 ; j<n ; j++){
+//         mpp[s[j]]++;
+        
+        
+//         while(mpp.size() < j-i+1){
+//                 mpp[s[i]]--;
+//                 if (mpp[s[i]] == 0) {
+//                   mpp.erase(s[i]);
+//                 }
+//                 i++;
+//             }
+        
+
+//         if(mpp.size()==j-i+1){
+//             min=max(min,j-i+1);
+//         }
+        
+//     }
+    
+//     return min;
